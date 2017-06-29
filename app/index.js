4189168e814c85a5de0efee8b18a37dfaba5727c
cavaloni <chad.avalon1@gmail.com>
2017-06-29 14:08:20 -0700

    mrg conflice fx

--- app/index.js
+++ app/index.js
@@ -1,15 +1,9 @@
 
-const React = require('react');
-const ReactDOM = require('react-dom');
-require('./index.css');
-<<<<<<< HEAD
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Day from './components/Day'
 import Timer from './components/Timer';
-=======
-const Day = require('./components/Day.js')
-const Timer = require('./components/Timer.js')
-
->>>>>>> 5829ca9a8fdfecb699ac7361ee8be9cccf38c79c
+require('./index.css');
 
 ReactDOM.render(
   <Day />,
