9c9e06e93c190b04b7f13ab1a8530048f9789469
fujisawakyle <fujisawakyle@gmail.com>
2017-08-29 20:22:25 -0700

    added model

---
+++ model/sessions.js
@@ -0,0 +1,14 @@
+//model/sessions.js
+'use strict';
+//import dependency
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//create new instance of the mongoose.schema. the schema takes an object that shows
+//the shape of your database entries.
+var SessionSchema = new Schema({
+  journal: String
+});
+
+//export our module to use in server.js
+module.exports = mongoose.model('Session', SessionSchema);
